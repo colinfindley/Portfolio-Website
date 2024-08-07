33f4d5308bfa1264a4f1105ef648ba80c67ea5dc
colinfindley <62814733+colinfindley@users.noreply.github.com>
2024-08-07 14:53:43 -0400

    Updated file structure

--- scripts.js
+++ scripts.js
@@ -9,3 +9,14 @@ function toggleSound(videoId) {
         button.textContent = "🔇";
     }
 }
+
+document.addEventListener('DOMContentLoaded', function() {
+    const currentLocation = location.href;
+    const menuItem = document.querySelectorAll('nav ul li a');
+    const menuLength = menuItem.length;
+    for (let i = 0; i < menuLength; i++) {
+        if (menuItem[i].href === currentLocation) {
+            menuItem[i].className = 'active';
+        }
+    }
+});
